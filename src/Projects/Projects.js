3094b610490e2bdc77b2b395b2c47fb9e61d62cf
tolulopeoduro <tolulopeoduro345@gmail.com>
2021-12-26 11:25:39 +0000

    initial commit

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -21,6 +21,15 @@ const Projects = () => {
                 <Project src = "https://res.cloudinary.com/dtuafcbbd/image/upload/v1640470479/personal/Screenshot_20.png"
                 name = "Sketch" link = "https://z5mo7.csb.app/" />
             </div>
+
+            <div className = {styles["project-list-mobile"]}>
+                <Project src = "https://res.cloudinary.com/dtuafcbbd/image/upload/v1640470479/personal/Screenshot_19.png"
+                name = "WORKFORCE" link = "https://workforce-01.netlify.app/" />
+                <Project src = "https://res.cloudinary.com/dtuafcbbd/image/upload/v1640470479/personal/Screenshot_17.png"
+                name = "Calculator" link = "https://4p8bk.csb.app/" />
+                <Project src = "https://res.cloudinary.com/dtuafcbbd/image/upload/v1640470479/personal/Screenshot_18.png"
+                name = "Movicheq" link = "https://movicheq.netlify.app/" />
+            </div>
         </div>
     )
 }
